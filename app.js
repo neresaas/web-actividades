2cfde10ef4a5b0350fc876b50fbc9b116f38b7ec
Nerea <neresaas@gmail.com>
2024-02-20 14:07:38 +0100

    ficheros data

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const app = require('express');
+const express = require('express');
+const app = express();
+const port = 8081;
+
+app.listen(port, () => {
+    console.log('Servidor activo en '+port)
+});
---
+++ data/activities.js
@@ -0,0 +1,6 @@
+let activities = [
+    { name: 'UNICEF', money: 0 },
+    { name: 'Save the children', money: 0 },
+    { name: 'WWF', money: 300 },
+];
+module.exports = activities;
